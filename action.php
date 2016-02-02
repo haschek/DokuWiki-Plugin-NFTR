f32bc66ab6b5c5f21c228ea02166a8b7025f93b2
Andreas Gohr <andi@splitbrain.org>
2016-02-02 16:12:23 +0100

    Adjust method signatures to match parent

--- action.php
+++ action.php
@@ -81,7 +81,7 @@ class action_plugin_nftr extends DokuWiki_Action_Plugin {
     /**
     * Register its handlers with the DokuWiki's event controller
     */
-    function register(&$controller)
+    function register(Doku_Event_Handler $controller)
     {
         // alter html/http header for meta data
         $controller->register_hook('TPL_METAHEADER_OUTPUT', 'BEFORE',  $this, 'setNoindex');
